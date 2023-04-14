db805d36fb2fe68440abdb39c7185668e9953124
PratikMane34 <manepratik7@gmail.com>
2023-04-14 20:05:26 +0530

    Strategy Builder and Trade Journal is created

---
+++ hsm/hsm_working/doctype/strategy_builder/strategy_builder.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Pratik Mane and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class StrategyBuilder(Document):
+	pass
---
+++ hsm/hsm_working/doctype/trade_journal/trade_journal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Pratik Mane and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Trade Journal', {
+	// refresh: function(frm) {
+
+	// }
+});
